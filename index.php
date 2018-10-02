ebcf702ebba9f978cdb87c3abd0e95b4640d839a
ejlarsson <e.johan.larsson@gmail.com>
2018-10-02 22:08:45 +0200

    x

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
 include_once 'header.php';
-  
-?>
 
+?>
+<br>MENU:<br>
+<br><a href="/bids.php">Bids</a>
+<br><a href="/exercises.php">Exercises</a>
+<br><a href="/quicklog.php">QuickLog</a>
