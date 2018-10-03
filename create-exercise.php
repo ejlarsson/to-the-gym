dea14f59250dccb8d4b0350dc6d570f899e517fb
ejlarsson <e.johan.larsson@gmail.com>
2018-10-03 22:30:41 +0200

    x

--- create-exercise.php
+++ create-exercise.php
@@ -5,15 +5,15 @@ if (session_id() == '' || !isset($_SESSION['user_uuid'])) {
 	header('Location: /'); //redirect to main
 } else {
 
-	if(isset($_POST['exercise_date'])) {
-		$date = $_POST['exercise_date'];
+	if(isset($_POST['date'])) {
+		$date = $_POST['date'];
 		if(isset($_POST['duration'])) $duration = $_POST['duration']; else $duration = NULL;
 		if(isset($_POST['exercise_type'])) $type = $_POST['exercise_type']; else $type = NULL;
 		$user_uuid = $_SESSION['user_uuid'];
 		
 		include_once 'sql.php';
 		
-		echo $date . " | " . $duration . " | " . $type . " | " . $user_uuid;
+		//echo $date . " | " . $duration . " | " . $type . " | " . $user_uuid;
 		
 		if(createExercise(getConnection(), $user_uuid, $date, $duration, $type)) {
 			echo "<noscript>Trening er registrert</noscript>";
