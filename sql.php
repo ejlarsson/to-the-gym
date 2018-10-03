c9738ba8cf904cfb2ca11087eb8c64c1b8181c52
ejlarsson <e.johan.larsson@gmail.com>
2018-10-03 14:41:41 +0200

    x

--- sql.php
+++ sql.php
@@ -61,9 +61,11 @@ function queryBids($conn, $user_uuid = NULL, $period = NULL) {
 
 function queryExercises($conn, $user_uuid = NULL, $period = NULL, $exercise_uuid = NULL) {
 	$query = 'SELECT eu.name AS user_name, eu.uuid AS user_uuid, p.name AS period, et.value AS exercise_type, e.exercise_duration_minutes AS exercise_duration, e.exercise_date AS exercise_date FROM ttg.exercise e INNER JOIN ttg.bid b ON b.id = e.bid_id INNER JOIN ttg.exercise_user eu ON eu.id = b.exercise_user_id INNER JOIN period p ON p.id = b.period_id LEFT JOIN ttg.exercise_type et ON et.id = e.exercise_type_id';
+	$arr = array(NULL, NULL, NULL);
+	
 	if (isset($user_uuid) OR isset($period) OR isset($exercise_uuid)) {
 		$query = $query . " WHERE ";
-		$arr = array(NULL, NULL, NULL);
+		
 		if(isset($user_uuid)) {
 			$query = $query . "eu.uuid = $1";
 			$arr[0] = $user_uuid;
