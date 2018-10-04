1c24ece1420d2d7fe751153ac1278b5c5a57c646
ejlarsson <e.johan.larsson@gmail.com>
2018-10-05 00:41:00 +0200

    x

--- sql.php
+++ sql.php
@@ -172,8 +172,8 @@ function queryUsers($conn, $user_uuid = NULL, $login = NULL) {
 	}
 	$query = $query . ' ORDER BY u.id ASC';
 	
-	echo isset($user_uuid) . ' = ' . $user_uuid . ' | ' . isset($login) . ' = ' . $login . ' | ' . $query;
-	exit;
+	echo isset($user_uuid) . ' = ' . $user_uuid . ' | ' . isset($login) . ' = ' . $login . ' | ' . $query . '<br>';
+	
 	
 	return pg_query_params($conn, $query, $arr);
 }
