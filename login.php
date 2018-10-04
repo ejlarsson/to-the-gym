d3c0e3ae737d996aec253060c0dcd03bb2e50999
ejlarsson <e.johan.larsson@gmail.com>
2018-10-04 23:05:41 +0200

    x

--- login.php
+++ login.php
@@ -10,6 +10,7 @@ if (isset($_POST['login'])) {
 		$res = validateUserPassword(getConnection(), $_POST['login'], NULL);
 	}
 	echo '<pre>'; print_r($res); echo '</pre>';
+	exit;
 	if ($res) {
 		$_SESSION['user_uuid'] = pg_fetch_result($res, 0, 0);
 		$_SESSION['user_name'] = pg_fetch_result($res, 0, 1);
