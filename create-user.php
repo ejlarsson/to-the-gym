f4f1812c5873517e08fd93ebee21276e6fe997aa
ejlarsson <e.johan.larsson@gmail.com>
2018-10-05 00:47:08 +0200

    x

--- create-user.php
+++ create-user.php
@@ -8,14 +8,20 @@ if (!empty($_POST['login']) && !empty($_POST['name'])) {
 	if(!empty($_POST['password'])) $password = $_POST['password'];
 	
 	if (createUser(getConnection(), $_POST['login'], $_POST['name'], $password) === TRUE) {
+		echo 'got here<br>';
+		
 		$users = queryUsers($conn, NULL, $_POST['login']);
 		$user = pg_fetch_row($res);
 		
 		$_SESSION['user_uuid'] = $user[1];
 		$_SESSION['user_name'] = $user[2];
 		
+		echo 'uuid:'.$_SESSION['user_uuid'].' name:'.$_SESSION['user_name']
+		
+		exit;
 		header('Location: /');
 	} else {
+		exit;
 		header('Location: /');
 	}
 }
