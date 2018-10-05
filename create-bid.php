6ce9716fc4f2841b71dc06a5064751783b2560f8
ejlarsson <e.johan.larsson@gmail.com>
2018-10-05 02:32:30 +0200

    x

--- create-bid.php
+++ create-bid.php
@@ -1,19 +1,16 @@
 <?php
 session_start(); //gets session id from cookies, or prepa
 
-if (session_id() == '' || !empty($_SESSION['user_uuid'])) {
+if (session_id() == '' || !isset($_SESSION['user_uuid'])) {
 	header('Location: /'); //redirect to main
 } else {
-		echo 'asdf';
 	if(!empty($_POST['bid'])) {
 		$bid = $_POST['bid'];
 		$user_uuid = $_SESSION['user_uuid'];
 		
 		include_once 'sql.php';
 		$res = createBid(getConnection(), $user_uuid, $bid);		
-		
-		echo 'asdf';
-		exit;
+
 		if($res) {
 			header('Location: /bids.php');
 		} else {
