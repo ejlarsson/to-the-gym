d34a0f8030504e286ceb3c78dde7e71a5c999541
ejlarsson <e.johan.larsson@gmail.com>
2018-10-05 16:38:46 +0200

    x

--- statistics.php
+++ statistics.php
@@ -23,6 +23,72 @@ if (session_id() == '' || !isset($_SESSION['user_uuid'])) {
 		<noscript>
 			<link rel="stylesheet" href="assets/css/noscript.css" />
 		</noscript>
+		<!-- Scripts -->
+		<script src="assets/js/jquery.min.js" />
+		<script src="assets/js/jquery.scrolly.min.js"/>
+		<script src="assets/js/browser.min.js"/>
+		<script src="assets/js/breakpoints.min.js"/>
+		<script src="assets/js/util.js"/>
+		<script src="assets/js/main.js"/>
+
+		<script src="assets/js/Chart.bundle.js"/>
+		<script src="assets/js/Chart.bundle.min.js"/>
+		
+		<script>
+
+		var data = {
+			labels: ['Arne', 'Bert', 'Carl', 'David'],
+			datasets: [{
+				label: 'Bid',
+				backgroundColor: 'rgba(255, 99, 132, 0.2)',
+				borderColor: 'rgba(255,99,132,1)',
+				borderWidth: 1,
+				data: [5, 10, 12, 5]
+			}, {
+				label: 'Exercised',
+				backgroundColor: 'rgba(54, 162, 235, 0.2)',
+				borderColor: 'rgba(54, 162, 235, 1)',
+				borderWidth: 1,
+				data: [2, 3, 4, 6]
+			}]
+
+		};
+
+		var ctx = document.getElementById('canvas').getContext('2d');
+
+		window.onload = function() {
+			window.myHorizontalBar = new Chart(ctx, {
+				type: 'horizontalBar',
+				data: data,
+				options: {
+					// Elements options apply to all of the options unless overridden in a dataset
+					// In this case, we are setting the border of each horizontal bar to be 2px wide
+					elements: {
+						rectangle: {
+							borderWidth: 2,
+						}
+					},
+					responsive: true,
+					legend: {
+						position: 'top',
+					},
+					title: {
+						display: true,
+						text: 'Exercised vs. bid'
+					},
+					scales: {
+						xAxes: [{
+							ticks: {
+								min: 0,
+								stepSize: 1
+							}
+						}]
+					}
+				}
+			});
+		};
+
+		</script>
 	</head>
 	<body>
 
@@ -66,74 +132,7 @@ if (session_id() == '' || !isset($_SESSION['user_uuid'])) {
 
 		<!-- Footer -->
 		<footer id="footer">
-		</footer>
-
-				<script src="assets/js/chart.bundle.js"/>
-		<script src="assets/js/chart.bundle.min.js"/>
-
-		<script>
-
-			var data = {
-				labels: ['Arne', 'Bert', 'Carl', 'David'],
-				datasets: [{
-					label: 'Bid',
-					backgroundColor: 'rgba(255, 99, 132, 0.2)',
-					borderColor: 'rgba(255,99,132,1)',
-					borderWidth: 1,
-					data: [5, 10, 12, 5]
-				}, {
-					label: 'Exercised',
-					backgroundColor: 'rgba(54, 162, 235, 0.2)',
-					borderColor: 'rgba(54, 162, 235, 1)',
-					borderWidth: 1,
-					data: [2, 3, 4, 6]
-				}]
-
-			};
-
-			var ctx = document.getElementById('canvas').getContext('2d');
-
-			window.onload = function() {
-				window.myHorizontalBar = new Chart(ctx, {
-					type: 'horizontalBar',
-					data: data,
-					options: {
-						// Elements options apply to all of the options unless overridden in a dataset
-						// In this case, we are setting the border of each horizontal bar to be 2px wide
-						elements: {
-							rectangle: {
-								borderWidth: 2,
-							}
-						},
-						responsive: true,
-						legend: {
-							position: 'top',
-						},
-						title: {
-							display: true,
-							text: 'Exercised vs. bid'
-						},
-						scales: {
-							xAxes: [{
-								ticks: {
-									min: 0,
-									stepSize: 1
-								}
-							}]
-						}
-					}
-				});
-			};
-
-		</script>
-		
-		<!-- Scripts -->
-		<script src="assets/js/jquery.min.js" />
-		<script src="assets/js/jquery.scrolly.min.js"/>
-		<script src="assets/js/browser.min.js"/>
-		<script src="assets/js/breakpoints.min.js"/>
-		<script src="assets/js/util.js"/>
-		<script src="assets/js/main.js"/>
+		</footer>		
 
 	</body>
 </html>
