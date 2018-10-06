a974773fee949b710ee5ede1e7625f0fbff4bd9c
ejlarsson <e.johan.larsson@gmail.com>
2018-10-06 22:03:30 +0200

    x

--- statistics.php
+++ statistics.php
@@ -85,8 +85,81 @@
 		<script src="assets/js/main.js"/>
 		
 		<script>
+
+function yourFunction(){
+	var ctx = document.getElementById('canvas').getContext('2d');
+	alert('asdf');
+	window.myHorizontalBar = new Chart(ctx, {
+		type: 'horizontalBar',
+		data: data,
+		options: {
+			// Elements options apply to all of the options unless overridden in a dataset
+			// In this case, we are setting the border of each horizontal bar to be 2px wide
+			elements: {
+				rectangle: {
+					borderWidth: 2,
+				}
+			},
+			responsive: true,
+			legend: {
+				position: 'top',
+			},
+			title: {
+				display: true,
+				text: 'Exercised vs. bid'
+			},
+			scales: {
+				xAxes: [{
+					ticks: {
+						min: 0,
+						stepSize: 1
+					}
+				}]
+			}
+		}
+	});
+}
+
+
+// ...
+// at the end of the file...
+window.addEventListener ? window.addEventListener("load",yourFunction,false) : window.attachEvent && window.attachEvent("onload",yourFunction);
+
+jQuery(document).ready(function yourFunction2(){
+	var ctx = document.getElementById('canvas').getContext('2d');
+	alert('asdf2');
+	window.myHorizontalBar = new Chart(ctx, {
+		type: 'horizontalBar',
+		data: data,
+		options: {
+			// Elements options apply to all of the options unless overridden in a dataset
+			// In this case, we are setting the border of each horizontal bar to be 2px wide
+			elements: {
+				rectangle: {
+					borderWidth: 2,
+				}
+			},
+			responsive: true,
+			legend: {
+				position: 'top',
+			},
+			title: {
+				display: true,
+				text: 'Exercised vs. bid'
+			},
+			scales: {
+				xAxes: [{
+					ticks: {
+						min: 0,
+						stepSize: 1
+					}
+				}]
+			}
+		}
+	});
+}); 
 		
-		window.onload = function() {
+/*		window.onload = function() {
 			var ctx = document.getElementById('canvas').getContext('2d');
 			alert('asdf');
 			window.myHorizontalBar = new Chart(ctx, {
@@ -118,7 +191,7 @@
 					}
 				}
 			});
-		};
+		};*/
 		
 		</script>
 
