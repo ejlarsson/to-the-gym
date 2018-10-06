17e11716d455105547bc62973a8f176c554fda1c
ejlarsson <e.johan.larsson@gmail.com>
2018-10-06 21:53:26 +0200

    x

--- statistics.php
+++ statistics.php
@@ -28,43 +28,6 @@
 
 		};
 
-/*		window.onload = function() {
-			var ctx = document.getElementById('canvas').getContext('2d');
-			alert('asdf');
-			window.myHorizontalBar = new Chart(ctx, {
-				type: 'horizontalBar',
-				data: data,
-				options: {
-					// Elements options apply to all of the options unless overridden in a dataset
-					// In this case, we are setting the border of each horizontal bar to be 2px wide
-					elements: {
-						rectangle: {
-							borderWidth: 2,
-						}
-					},
-					responsive: true,
-					legend: {
-						position: 'top',
-					},
-					title: {
-						display: true,
-						text: 'Exercised vs. bid'
-					},
-					scales: {
-						xAxes: [{
-							ticks: {
-								min: 0,
-								stepSize: 1
-							}
-						}]
-					}
-				}
-			});
-		};*/
-		
-		window.onload = function() {
-			alert('asdf2');
-		};
 
 		</script>
 	</head>
@@ -120,6 +83,44 @@
 		<script src="assets/js/breakpoints.min.js"/>
 		<script src="assets/js/util.js"/>
 		<script src="assets/js/main.js"/>
+		
+		<script>
+		
+		window.onload = function() {
+			var ctx = document.getElementById('canvas').getContext('2d');
+			alert('asdf');
+			window.myHorizontalBar = new Chart(ctx, {
+				type: 'horizontalBar',
+				data: data,
+				options: {
+					// Elements options apply to all of the options unless overridden in a dataset
+					// In this case, we are setting the border of each horizontal bar to be 2px wide
+					elements: {
+						rectangle: {
+							borderWidth: 2,
+						}
+					},
+					responsive: true,
+					legend: {
+						position: 'top',
+					},
+					title: {
+						display: true,
+						text: 'Exercised vs. bid'
+					},
+					scales: {
+						xAxes: [{
+							ticks: {
+								min: 0,
+								stepSize: 1
+							}
+						}]
+					}
+				}
+			});
+		};
+		
+		</script>
 
 	</body>
 </html>
